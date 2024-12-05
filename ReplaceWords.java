c817182fe25f1a9f42be771856ca310197c3d303
firoz Syed <syedfiroz2010@gmail.com>
2024-12-04 23:49:41 -0800

    code fix

--- ReplaceWords.java
+++ ReplaceWords.java
@@ -29,3 +29,65 @@ class Solution {
 
     }
 }
+
+// Approach : Using Trie
+// Time : O(nl) + O(ml) + O(ml) // n- Total no of words in dict insert into Trie & split m words in sentence of len l & search
+// space : O(nl) + O(ml) // store the words of dict in Trie & store the words of sentence in a temp array
+class Solution {
+    TrieNode root;
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+        public TrieNode(){
+            this.children = new TrieNode[26];
+        }
+    }
+
+    private void insert(String word){
+        TrieNode curr = this.root; // take curr pointer on root of the Trie
+        for(char ch:word.toCharArray()){
+            if(curr.children[ch-'a']== null){
+                curr.children[ch-'a'] = new TrieNode();
+            }
+            curr = curr.children[ch-'a'];
+        }
+        curr.isEnd = true;
+        return;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        StringBuilder result = new StringBuilder(); // for building result
+        this.root = new TrieNode(); // create object for Trie Node
+        // insert all words of Dict into Trie
+        for(String word: dictionary){
+            insert(word);
+        }
+
+        // split the sentence into words
+        String[] splitArr = sentence.split(" "); // split on spaces
+        // search each word in Trie and see if there is shortest version
+        for(int i =0;i<splitArr.length;i++){
+            if(i>0) result.append(" ");
+            result.append(getShortestVersion(splitArr[i]));
+        }
+
+        return result.toString();
+    }
+
+    private String getShortestVersion(String word){
+        StringBuilder result = new StringBuilder();
+        TrieNode curr = this.root;
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch - 'a'] == null || curr.isEnd){
+                break;
+            } else{
+                curr= curr.children[ch-'a'];
+                result.append(ch);
+            }
+        }
+        if(curr.isEnd){
+            return result.toString();
+        } 
+        return word;
+    }
+}
